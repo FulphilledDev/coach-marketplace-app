e2375fbc8fdaf4fe7942e192971c06b6d45a1ffe
Philip Simpson <dev.phits@gmail.com>
2022-09-29 15:39:17 -0700

    Email and Password form, Forgot Password Link, Sign In Button

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -13,9 +13,62 @@ function SignIn() {
 
   const navigate = useNavigate()
 
+  const onChange = () => {}
+
   return (
     <>
-      <h1>Sign In</h1>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">
+            Welcome Back!
+          </p>
+        </header>
+
+        <form>
+          <input 
+            type="email" 
+            className="emailInput" 
+            placeholder="Email" 
+            id="email" 
+            value={email} 
+            onChange={onChange} 
+          />
+
+          <div className="passwordInputDiv">
+            <input 
+              type={showPassword ? 'text' : 'password'} 
+              className='passwordInput' 
+              placeholder='Password' 
+              id='password' 
+              value={password} 
+              onChange={onChange} 
+            />
+
+            <img 
+              src={visibilityIcon} 
+              alt="show password" 
+              className="showPassword" 
+              onClick={() =>
+                setShowPassword((prevState) => !prevState)
+              }
+            />
+          </div>
+
+          <Link to='/forgot-password' className='forgotPasswordLink'>
+            Forgot Password?
+          </Link>
+
+          <div className="signInBar">
+            <p className="signInText">
+              Sign In
+            </p>
+            <button className="signInButton">
+              <ArrowRightIcon fill='#fff' width='34px' height='34px' />
+            </button>
+          </div>
+        </form>
+
+      </div>
     </>
   )
 }
