0009fb32ad61b7df7c6fbc3a255438060704a2a1
Philip Simpson <dev.phits@gmail.com>
2022-09-30 18:24:23 -0700

    Explore Page: Add Category Page and Link

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,8 +1,30 @@
+import { Link } from 'react-router-dom'
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
+
 function Explore() {
   return (
-    <>
-      <h1>Explore</h1>
-    </>
+    <div className='Explore'>
+      <header>
+        <p className="pageHeader">Explore</p>
+
+        <main>
+          {/* Slider */}
+
+          <p className="exploreCategoryHeading">Categories</p>
+          <div className="exploreCategories">
+            <Link to='/category/rent'>
+              <img src={rentCategoryImage} alt="rent" className='exploreCategoryImg' />
+              <p className="exploreCategoryName">Places for Rent</p>
+            </Link>
+            <Link to='/category/sell'>
+              <img src={sellCategoryImage} alt="sell" className='exploreCategoryImg' />
+              <p className="exploreCategoryName">Places for Sale</p>
+            </Link>
+          </div>
+        </main>
+      </header>
+    </div>
   )
 }
 
