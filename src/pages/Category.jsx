ec1adfd5e5e0929f3d356a602f145eb8be3dc433
Philip Simpson <dev.phits@gmail.com>
2022-09-30 18:37:04 -0700

    Category imports, useState, params, and fetchListings w/ query

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,5 +1,47 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { collection, getDocs, query, where, orderBy, limit, startAfter } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
+import Spinner from '../components/Spinner'
+
 
 function Category() {
+    const [ listings, setListings ] = useState(null)
+    const [ loading, setLoading ] = useState(true)
+
+    const params = useParams()
+
+    useEffect(() => {
+        const fetchListings = async () => {
+            try {
+                // Get Reference (to collection, not Doc)
+                const listingsRef = collection(db, 'listings')
+
+                // Create a query
+                const q = query(
+                    listingsRef, 
+                    where('type', '==', params.categoryName), 
+                    orderBy('timestamp', 'desc'), 
+                    limit(10)
+                )
+
+                // Execute a query
+                const querySnap = await getDocs(q)
+
+                let listings = []
+
+                querySnap.forEach((doc) => {
+                    console.log(doc.data())
+                })
+            } catch (error) {
+                
+            }
+        }
+
+        fetchListings()
+    })
+
   return (
     <div>Category</div>
   )
